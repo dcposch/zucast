1c4537df499b0fa227c03cac4390bb41e3b1c62e
DC <dcposch@dcpos.ch>
2023-05-12 17:00:54 +0200

    No overscroll during Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -16,12 +16,18 @@ export function Modal({
   // Close on escape
   useEscape(onClose);
 
-  // Freeze scroll on modal popup
+  // Preserve scroll on modal popup
   const [offset, setOffset] = useState<CSSProperties>();
   useEffect(() => {
     const top = document.documentElement.scrollTop || document.body.scrollTop;
     setOffset({ marginTop: top });
   }, []);
+
+  // No overscroll while modal is open
+  useEffect(() => {
+    document.documentElement.classList.add("no-overscroll");
+    return () => document.documentElement.classList.remove("no-overscroll");
+  });
 
   return (
     <div className="w-full h-full min-h-screen absolute left-0 top-0 bg-[rgba(0,0,0,0.5)] py-4 z-10 overflow-scroll">
